d88d2375e9a226a198a8cb2e0ffea90bb2e2fde2
Lordwill1 <messagelordwill@gmail.com>
2024-05-29 19:58:06 +0100

    working on css

---
+++ index.php
@@ -0,0 +1,57 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student management system</title>
+    <link rel="stylesheet" href="style.css">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav>
+        <label class="logo">W-School</label>
+
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="" class="btn btn-success">Login</a></li>
+            <button type="button" class="btn btn-primary active">Signup</button>
+        </ul>
+    </nav>
+
+    <div class="section1">
+        <label class="img_text">
+            We Teach Students With Care
+        </label>
+        <img class="main_img" src="school.png" alt="school front view">
+    </div>
+
+    <div class="containter">
+
+        <div class="row">
+
+            <div class="col-md-4">
+
+                <img src="classroom.jpg" alt="Classroom">
+
+            </div>
+
+        </div>
+
+        <div>
+        <div class="col-md-8">
+
+        </div>
+
+    </div>
+    
+</body>
+</html>
