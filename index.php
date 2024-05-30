b7b47ff0fa4bd5262811ed5e52fbd7ec94bf009c
Lordwill1 <messagelordwill@gmail.com>
2024-05-30 02:33:23 +0100

    class

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <nav>
         <label class="logo">W-School</label>
@@ -34,23 +35,61 @@
         <img class="main_img" src="school.png" alt="school front view">
     </div>
 
-    <div class="containter">
+    <div class="container">
 
         <div class="row">
 
             <div class="col-md-4">
+            <img class="welcome_img" src="schoolface.png" alt="Classroom">
+        
 
-                <img src="classroom.jpg" alt="Classroom">
+            </div>
 
+            <div class="col-md-8">
+
+             <h1>Welcome to W-School</h1>
+
+                <p>
+                W International School is a co-educational institution. 
+                W International School specializes in producing individuals of international repute. 
+                To this end, the curriculum of GIS is structured to prepare students for global success. 
+                Both teachers and students work as a team to ensure that students achieve their full potential. 
+                We inspire our students to become excellent individuals who enjoy life and contribute to the lives of others. 
+                With our state of the art hostels, classes and more, our students feel at home, though, away from home. 
+                All students are encouraged to take an active part in the life of the school, participating in sporting, academic and national events. 
+                Students are encouraged to take responsibilities for their actions, and to behave in a mature and sensible way at all times, both in the school and outside the school.
+                </p>
             </div>
 
         </div>
 
-        <div>
-        <div class="col-md-8">
+    
 
+    </div>
+
+    <center>
+        <h1>Our Teachers</h1>
+    </center>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="teacher" src="teacher1.jpg">
+                <p>My name is john</p>
+
+            </div>
+
+            <div class="col-md-4">
+            <img class="teacher" src="teacher3.jpg">
+
+            </div>
+
+            <div class="col-md-4">
+            <img class="teacher" src="teacher2.jpg">
+
+            </div>
+            
         </div>
-
     </div>
     
 </body>
